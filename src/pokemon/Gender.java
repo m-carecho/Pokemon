130aa5f9b25c2bac69abecfb028c256fc6962a66
yan-rds <yyan1207@gmail.com>
2022-02-19 15:07:14 -0300

    -feat Apply colors to Genders

--- src/pokemon/Gender.java
+++ src/pokemon/Gender.java
@@ -1,5 +1,12 @@
 package pokemon;
 
 public enum Gender {
-    MALE, FEMALE, OTHERS
+    MALE("\033[1;34m"), FEMALE("\033[1;35m"), OTHERS("\033[1;33m");
+
+    String cor;
+
+    Gender(String cor) {
+    this.cor = cor;
+    }
+
 }
