16e8902a7e238cb4f1fbd6b25c8e9a7d6a77e124
yan-rds <yyan1207@gmail.com>
2022-02-19 15:05:02 -0300

    -feat Create mapCreation method

--- src/pokemon/GameSystem.java
+++ src/pokemon/GameSystem.java
@@ -3,30 +3,20 @@ package pokemon;
 import java.util.ArrayList;
 import java.util.List;
 
-public class System {
-    private static List<Map> mapList = new ArrayList<>();
+public class GameSystem {
 
-    public void mapCreation(String name, MapType mapType){
+    public static List<Map> mapList = new ArrayList<>();
+
+    public static void mapCreation(String name, MapType mapType, List<Terrain> terrainList) {
         Map map = new Map();
         map.setName(name);
         map.setMapType(mapType);
-        while (map.getTerrains().size() < 9) {
-            map.getTerrains().add(terrainCreation(false, true, true, mapType));
-        }
+        map.setTerrains(terrainList);
+
         mapList.add(map);
-    }
-
-    public Terrain terrainCreation(boolean playerPresent, boolean itemSpawn, boolean pokemonSpawn, MapType mapType){
-            Terrain terrain = new Terrain();
-            terrain.setPlayerPresent(playerPresent);
-            terrain.setItemSpawnable(itemSpawn);
-            terrain.setMapType(mapType);
-            terrain.setPokemonSpawnable(pokemonSpawn);
-            return terrain;
-    }
-
-    public void mapMovement(Map map){
 
     }
+
+
 
 }
