c58f2ae8a9b4365de14c6dd5133a76aae3b679d1
yan-rds <yyan1207@gmail.com>
2022-02-19 15:34:32 -0300

    -feat mapMovement Method

--- src/pokemon/GameSystem.java
+++ src/pokemon/GameSystem.java
@@ -18,14 +18,41 @@ public class GameSystem {
     }
 
     public static Terrain terrainCreation(boolean playerPresent, boolean itemSpawn, boolean pokemonSpawn, MapType mapType,
-                                          String description) {
+                                          String description, String index) {
         Terrain terrain = new Terrain();
         terrain.setPlayerPresent(playerPresent);
         terrain.setItemSpawnable(itemSpawn);
         terrain.setMapType(mapType);
         terrain.setPokemonSpawnable(pokemonSpawn);
         terrain.setDescription(description);
+        terrain.setIndex(index);
         return terrain;
+    }
+
+    public static void mapMovement(Map map, int movement) {
+        for (Terrain terrain : map.getTerrains())
+            terrain.setPlayerPresent(false);
+        map.getTerrains().get(movement).setPlayerPresent(true);
+        for (Terrain terrain : map.getTerrains()) {
+            if (terrain.isPlayerPresent())
+                terrain.setUpdatedIndex(Gender.MALE.cor+"X"+"\033[0;38m");
+            else terrain.setUpdatedIndex(terrain.getIndex());
+        }
+        System.out.println(map.getTerrains().get(0).getDescription() + " ["+map.getTerrains().get(0)
+                .getUpdatedIndex()+"]\t\t\t" + map.getTerrains().get(1).getDescription() + " ["+map.getTerrains()
+                .get(1).getUpdatedIndex()+"]\t\t\t" + map.getTerrains().get(2).getDescription() + " ["+map.getTerrains()
+                .get(2).getUpdatedIndex()+"]");
+        System.out.println();
+        System.out.println(map.getTerrains().get(3).getDescription() + " ["+map.getTerrains().get(3)
+                .getUpdatedIndex()+"]\t\t" + map.getTerrains().get(4).getDescription() + " ["+map.getTerrains().get(4)
+                .getUpdatedIndex()+"]\t\t\t" + map.getTerrains().get(5).getDescription() + " ["+map.getTerrains()
+                .get(5).getUpdatedIndex()+"]");
+        System.out.println();
+        System.out.println(map.getTerrains().get(6).getDescription() + " ["+map.getTerrains().get(6)
+                .getUpdatedIndex()+"]\t\t\t" + map.getTerrains().get(7).getDescription() + " ["+map.getTerrains().get(7)
+                .getUpdatedIndex()+"]\t\t\t" + map.getTerrains().get(8).getDescription() + " ["+map.getTerrains().get(8)
+                .getUpdatedIndex()+"]");
+        System.out.println();
     }
 
 
